f483191e57bce0dd539bf9fdcfdc3c3842d18bf3
Vishnuvardhan Reddy <39486314+vvr3ddy@users.noreply.github.com>
2020-06-24 10:32:49 +0530

    :closed_book: Basics : 01.py
    escape characters

---
+++ Challenges/Basics/01.py
@@ -0,0 +1,13 @@
+"""
+Copyright 2020. 
+This is a python problem solution.
+Author : Vishnuvardhan Reddy
+Python Basics : Problem 1
+"""
+
+print("Twinkle, twinkle, little star,"
+        "\n\tHow I wonder what you are!"
+        "\n\t\tUp above the world so high,"
+        "\n\t\t\tLike a diamond in the sky."
+        "\nTwinkle, twinkle, little star,"
+	    "\n\tHow I wonder what you are")
