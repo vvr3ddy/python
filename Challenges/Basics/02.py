dc1cd7dc1ae5f276b8ca394adcc83b70d513b2d5
Vishnuvardhan Reddy <39486314+vvr3ddy@users.noreply.github.com>
2020-06-24 10:39:30 +0530

    :closed_book: Basics : 02.py
    Display python version info
    
    Python Version
    3.6.9 (default, Apr 18 2020, 01:56:04) 
    [GCC 8.4.0]
    Version info
    sys.version_info(major=3, minor=6, micro=9, releaselevel='final', serial=0)

---
+++ Challenges/Basics/02.py
@@ -0,0 +1,11 @@
+"""
+Copyright 2020. 
+This is a python problem solution.
+Author : Vishnuvardhan Reddy
+Python Basics : Problem 2
+"""
+import sys
+print("Python Version")
+print(sys.version)
+print("Version info")
+print(sys.version_info)
