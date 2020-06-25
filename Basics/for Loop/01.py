797ee38a502946255233d96b5b1a3809da7714f5
Vishnuvardhan Reddy <39486314+vvr3ddy@users.noreply.github.com>
2020-06-25 10:44:27 +0530

    Basics/for Loop : Create 01.py
    A For loop to print numbers from 1 to a specified value n

---
+++ Basics/for Loop/01.py
@@ -0,0 +1,9 @@
+"""
+Copyright 2020. 
+This is a python problem solution.
+Author : Vishnuvardhan Reddy
+Python Basics - For Loops : Problem 1
+"""
+n =  int(input("Enter a natural number :"))
+for i in range(n):
+    print(i, end="\n")
