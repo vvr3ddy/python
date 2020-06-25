cc4cccb1f819f133700efe08355c0368d6d97105
Vishnuvardhan Reddy <39486314+vvr3ddy@users.noreply.github.com>
2020-06-25 10:55:51 +0530

    Basics/for Loop/02.py: improve the code
    add support for float point range

--- Basics/for Loop/02.py
+++ Basics/for Loop/02.py
@@ -2,15 +2,16 @@
 Copyright 2020. 
 This is a python problem solution.
 Author : Vishnuvardhan Reddy
-Python Basics - For Loops : Problem 2
+Python Basics - For Loops : Problem 1
 """
+from numpy import arange
 
 start =  int(input("Enter start value :"))
 stop = int(input("Enter stop value:"))
-step = int(input("Enter the step size"))
+step = float(input("Enter the step size:"))
 if abs(start - stop) < step:
     print("Increase the range or decrease the step size...")
     exit()
 else :
-    for i in range(start, stop, step):
+    for i in arange(start, stop, step):
         print(i, end="\n")
